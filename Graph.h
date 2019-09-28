491c760ed6a534cdd02a2b54c0be2046c2c86bc0
Alexandr Maximenko <maksimenko.ae@phystech.edu>
2019-09-28 17:34:17 +0300

    headers added

---
+++ Graph.h
@@ -0,0 +1,31 @@
+#pragma once
+#include<vector>
+class Graph
+{
+public:
+	typedef size_t Vertex;
+	Graph(size_t vertex_count, bool is_directed)
+	{
+		vertex_count_ = vertex_count;
+		is_directed_ = is_directed;
+	}
+
+	size_t getVertexCount() const
+	{
+		return vertex_count_;
+	}
+
+	size_t geEdgeCount() const
+	{
+		return edge_count_;
+	}
+
+	virtual std::vector<Vertex> getNeighbors(const Vertex& v) const = 0;
+
+	virtual void addEdge(const Vertex& start, const Vertex& finish) = 0;
+
+protected:
+	bool is_directed_;
+	size_t vertex_count_,
+		edge_count_;
+};
