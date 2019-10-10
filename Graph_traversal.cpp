6622125b9b4e6535789cc89308b0d5cefc506da2
Alexandr Maximenko <maksimenko.ae@phystech.edu>
2019-10-10 21:22:06 +0300

    Замечания исправлены

--- Graph_traversal.cpp
+++ Graph_traversal.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-
+#define NO_EDGE 0
+#define IS_EDGE 1
 //Graph_traversal "https://informatics.mccme.ru/mod/statements/view3.php?id=256&chapterid=164#1"
 
 class Graph
@@ -33,11 +34,8 @@ public:
 	virtual size_t getNeighborsCount(const Vertex&  v) const = 0;
 protected:
 	bool is_directed_;
-	size_t vertex_count_,
-		   edge_count_;
-
-
-
+	size_t vertex_count_ = 0,
+		edge_count_ = 0;
 };
 
 class GraphAdjMatrix : public Graph
@@ -46,24 +44,24 @@ public:
 	GraphAdjMatrix(const size_t vertex_count, bool is_directed) :
 		Graph(vertex_count, is_directed)
 	{
-		adj_matrix_= std::vector<std::vector<int>>(vertex_count, std::vector<int>(vertex_count, 0));
+		adj_matrix_ = std::vector<std::vector<int>>(vertex_count, std::vector<int>(vertex_count, NO_EDGE));
 
 	}
 
 	void addEdge(const Vertex& start, const Vertex& finish) override
 	{
 		Graph::addEdge(start, finish);
-		adj_matrix_[start][finish] = 1;
+		adj_matrix_[start][finish] = IS_EDGE;
 		if (is_directed_)
 		{
-			adj_matrix_[finish][start] = 1;
+			adj_matrix_[finish][start] = IS_EDGE;
 		}
 	}
 
 	std::vector<Vertex> getNeighbors(const Vertex& v) const override
 	{
 		std::vector<Vertex> neighbors;
-		for (size_t i = 0; i < vertex_count_; i++)
+		for (Vertex i = 0; i < vertex_count_; i++)
 		{
 			if (adj_matrix_[v][i])
 			{
@@ -76,8 +74,13 @@ public:
 	size_t getNeighborsCount(const Vertex&  v) const override
 	{
 		size_t count = 0;
-		for (auto i : adj_matrix_[v])
-			count += i;
+		for (Vertex i = 0; i < adj_matrix_[v].size(); i++)
+		{
+			if (adj_matrix_[v][i] == IS_EDGE)
+			{
+				count++;
+			}
+		}
 		return count;
 	}
 
@@ -101,44 +104,42 @@ public:
 		}
 	}
 
-
-
-
 private:
 	std::vector<std::vector<int>> adj_matrix_;
 
 };
 
-namespace GraphProcessing 
+namespace GraphProcessing
 {
 	enum VertexMark
 	{
 		WHITE, GREY, BLACK
 	};
 
-	
+	typedef size_t Vertex;
+
 	void dfs_visit(const Graph& g, const size_t& vertex, size_t& counter, std::vector<VertexMark>& vertex_marks)
 	{
 		counter++;
 		vertex_marks[vertex] = GREY;
-		for (size_t i : g.getNeighbors(vertex))
+		for (Vertex neighbor : g.getNeighbors(vertex))
 		{
-			if (vertex_marks[i] == WHITE)
+			if (vertex_marks[neighbor] == WHITE)
 			{
-				dfs_visit(g, i, counter, vertex_marks);
-				vertex_marks[i] = BLACK;
+				dfs_visit(g, neighbor, counter, vertex_marks);
+				vertex_marks[neighbor] = BLACK;
 			}
 		}
 
 	}
-	
-	
-	void CountVerticesInConnectivityComponent(const Graph& g, const size_t& vertex, size_t& counter)
+
+	size_t getComponentSize(const Graph& g, const size_t& vertex)
 	{
+		size_t counter = 0;
 		std::vector<VertexMark> vertex_marks(g.getVertexCount(), WHITE);
 		dfs_visit(g, vertex, counter, vertex_marks);
+		return counter;
 	}
-
 }
 
 int main()
@@ -156,9 +157,5 @@ int main()
 				g.addEdge(i, j);
 		}
 	}
-	size_t answer = 0;
-	GraphProcessing::getConnectedCount(g, v - 1, answer);
-	std::cout << answer;
-	
-
+	std::cout << GraphProcessing::getComponentSize(g, v - 1);
 }
