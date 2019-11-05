6efa148118a0f592f927edb0cf8e6c40cdd92ea3
Alexandr Maximenko <maksimenko.ae@phystech.edu>
2019-11-05 22:50:54 +0300

    Замечания исправлены

--- task_F.cpp
+++ task_F.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
-
 class Graph
 {
 public:
@@ -33,6 +31,8 @@ public:
 	}
 
 	virtual size_t getNeighborsCount(const Vertex&  v) const = 0;
+
+	virtual void transposeGraph() = 0;
 
 protected:
 	bool is_directed_;
@@ -70,16 +70,17 @@ public:
 		return adj_list_[v].size();
 	}
 
-	void printGraph() const
+	void transposeGraph() override
 	{
-		std::cout << "Print\n";
-		std::cout << vertex_count_;
-		for (size_t i = 0; i < vertex_count_; i++)
+		std::vector<std::vector<Vertex>> new_list(vertex_count_);
+		for (Vertex i = 0; i < vertex_count_; i++)
 		{
-			for (auto k : adj_list_[i])
-				std::cout << k << " ";
-			std::cout << std::endl;
+			for (Vertex vertex : adj_list_[i])
+			{
+				new_list[vertex].push_back(i);
+			}
 		}
+		adj_list_ = new_list;
 	}
 
 private:
@@ -136,7 +137,7 @@ namespace GraphProcessing
 			}
 		}
 	}
-	size_t determineComponents(const Graph& g, const Graph& transposed_g, std::vector<Graph::Vertex>& vertex_component_number)
+	size_t determineComponents(Graph& g, std::vector<Graph::Vertex>& vertex_component_number)
 	{
 		size_t time = 0;
 		std::vector<size_t> finish_time(g.getVertexCount(), WASNT_VISIT);
@@ -146,13 +147,15 @@ namespace GraphProcessing
 		vertex_marks.clear();
 		vertex_marks = std::vector<VertexMark>(g.getVertexCount(), WHITE);
 		size_t component_count = 0;
+		g.transposeGraph();
+		std::reverse(order.begin(), order.end());
 		for (size_t i = 0; i < g.getVertexCount(); i++)			//dfs for transposed graph
 		{
-			Graph::Vertex current = order[g.getVertexCount() - i - 1];
+			Graph::Vertex current = order[i];
 			if (vertex_marks[current] == WHITE)
 			{
 				component_count++;
-				setComponentNumber(transposed_g, current, vertex_marks, component_count, vertex_component_number);
+				setComponentNumber(g, current, vertex_marks, component_count, vertex_component_number);
 			}
 		}
 		return component_count;
@@ -164,16 +167,14 @@ int main()
 	size_t n, m;
 	std::cin >> n >> m;
 	GraphAdjList g(n, true);
-	GraphAdjList transoped_g(n, true);
 	for (size_t i = 0; i < m; i++)
 	{
 		Graph::Vertex from, to;
 		std::cin >> from >> to;
 		g.addEdge(from - 1, to - 1);
-		transoped_g.addEdge(to - 1, from - 1);
 	}
 	std::vector<Graph::Vertex> vertex_component_number(g.getVertexCount(), 0);
-	std::cout << GraphProcessing::determineComponents(g, transoped_g, vertex_component_number) << std::endl;
+	std::cout << GraphProcessing::determineComponents(g, vertex_component_number) << std::endl;
 	for (Graph::Vertex i : vertex_component_number)
 	{
 		std::cout << i << " ";
